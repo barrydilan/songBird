2f7a6d93303ed774eccc07d5b918bfcaceb1f89d
queharambe <queharambe@gmail.com>
2022-11-12 16:13:53 +0100

    feat: color change on answears

--- app/js/app.js
+++ app/js/app.js
@@ -1,11 +1,19 @@
 import birdsData from "./birdsData.js";
 
-const victorineVariants = Array.from(document.querySelectorAll(".victorine-answers__variants-list__variant"));
-const infoPic = document.querySelector(".victorine-answers__victorine-info__pic");
-const infoName = document.querySelector(".victorine-answers__victorine-info__name");
+const victorineVariants = Array.from(
+  document.querySelectorAll(".victorine-answers__variants-list__variant")
+);
+const infoPic = document.querySelector(
+  ".victorine-answers__victorine-info__pic"
+);
+const infoName = document.querySelector(
+  ".victorine-answers__victorine-info__name"
+);
 const infoSound = document.querySelector(".info-audio");
 const nextBtn = document.querySelector(".victorine-button__button");
-const themeBtns = Array.from(document.querySelectorAll(".victorine-selector__theme"))
+const themeBtns = Array.from(
+  document.querySelectorAll(".victorine-selector__theme")
+);
 const correctAudioEl = document.querySelector(".correct-audio");
 const correctPicEl = document.querySelector(".victorine-question__pic");
 const correctNameEl = document.querySelector(".victorine-question__name");
@@ -13,113 +21,101 @@ let scoreTable = document.querySelector(".header__score");
 let score = 0;
 let tryCounter = 0;
 let theme = 0;
-let correctName = '';
-
-
+let correctName = "";
 
 nextBtn.addEventListener("click", nextRound);
 
-
 function nextRound() {
-    theme += 1;
-    if (theme>5) {
-        endGame();
-        return
-    }
-    loadVictorine();
+  theme += 1;
+  if (theme > 5) {
+    endGame();
+    return;
+  }
+  loadVictorine();
 }
-
-
-function updateCorrectVictorine() {
-    for (let i=0; i<birdsData[theme].length; i++) {
-        if (birdsData[theme][i].name == correctName) {
-            correctPicEl.src = birdsData[theme][i].image;
-            correctNameEl.innerText = birdsData[theme][i].name;
-        }
-    }
-}
-
-function updateAnswerInfo(variant){
-    for (let i=0; i<birdsData[theme].length; i++) {
-    if (birdsData[theme][i].name == variant) {
-        infoPic.src = birdsData[theme][i].image;
-        infoName.innerText = birdsData[theme][i].name;
-        infoSound.setAttribute("style", "visibility: visible")
-        infoSound.src = birdsData[theme][i].audio;
-    }
-}
-}
-function infoUpdate(variant) {
-    updateAnswerInfo(variant);
-}
-
-
-
 
 function endGame() {
-    theme = 0;
-    themeBtns[5].classList.remove("victorine-selector__theme--chosen");
-    console.log("game over")
-    score = 0;
-    loadVictorine()
+  theme = 0;
+  themeBtns[5].classList.remove("victorine-selector__theme--chosen");
+  console.log("game over");
+  score = 0;
+  loadVictorine();
 }
-
-
-function changeThemeBtnStyle(theme) {
-    themeBtns[theme].classList.remove("victorine-selector__theme--chosen");
-}
-
 
 function checkAnswer(eventObj) {
-    let scoreUpdate = 0;
-    tryCounter++;
-    eventObj.preventDefault();
-    let clickedEl = eventObj.target;
-    infoUpdate(clickedEl.innerText);
-    if (clickedEl.innerText == correctName) {
-        updateCorrectVictorine();
-        scoreUpdate = 6 - tryCounter;
-        tryCounter = 0;
-        score += scoreUpdate;
-        scoreTable.innerText = "Score: " + score;
-        nextBtn.removeAttribute("disabled");
-        return scoreUpdate;
-    }
-   
+  let scoreUpdate = 0;
+  tryCounter++;
+  eventObj.preventDefault();
+  let clickedEl = eventObj.target;
+  infoUpdate(clickedEl.innerText);
+  if (clickedEl.innerText == correctName) {
+    updateCorrectVictorine();
+    scoreUpdate = 6 - tryCounter;
+    tryCounter = 0;
+    score += scoreUpdate;
+    scoreTable.innerText = "Score: " + score;
+    clickedEl.setAttribute("style", "color:green")
+    nextBtn.removeAttribute("disabled");
+    return scoreUpdate;
+  }
+  if (nextBtn.hasAttribute("disabled")) clickedEl.setAttribute("style", "color:red");
 }
-
 
 function loadVictorine() {
-    for (let i=0; i<victorineVariants.length; i++) {
-        victorineVariants[i].innerText = birdsData[theme][i].name;
-        victorineVariants[i].addEventListener("click", checkAnswer)
-    }
-    scoreTable.innerText = "Score: " + score;
-    let correct = getRandomInt(6);
-    correctName = getRandomQuestion(correct);
-    correctNameEl.innerText = "*****";
-    correctPicEl.src = "/app/src/bird-outline.png";
-    infoPic.src = "/app/src/bird-outline.png";
-    infoName.innerText = "Choose answer";
-    infoSound.setAttribute("style", "visibility: hidden");
-    themeBtns[theme].classList.add("victorine-selector__theme--chosen");
-    if (theme>0) changeThemeBtnStyle(theme-1);
-    nextBtn.setAttribute("disabled", "true")
-    
+  for (let i = 0; i < victorineVariants.length; i++) {
+     victorineVariants[i].removeAttribute("style");
+    victorineVariants[i].innerText = birdsData[theme][i].name;
+    victorineVariants[i].addEventListener("click", checkAnswer);
+  }
+  scoreTable.innerText = "Score: " + score;
+  let correct = getRandomInt(6);
+  correctName = getRandomQuestion(correct);
+  correctNameEl.innerText = "*****";
+  correctPicEl.src = "/app/src/bird-outline.png";
+  infoPic.src = "/app/src/bird-outline.png";
+  infoName.innerText = "Choose answer";
+  infoSound.setAttribute("style", "visibility: hidden");
+  themeBtns[theme].classList.add("victorine-selector__theme--chosen");
+  if (theme > 0) changeThemeBtnStyle(theme - 1);
+  nextBtn.setAttribute("disabled", "true");
 }
-
 
 function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-  }
-
-function getRandomQuestion(correct) {
-    let correctName = birdsData[theme][correct].name;
-    correctAudioEl.src = birdsData[theme][correct].audio;
-    console.log(birdsData[theme][correct].name);
-    return correctName;
+  return Math.floor(Math.random() * max);
 }
 
+function getRandomQuestion(correct) {
+  let correctName = birdsData[theme][correct].name;
+  correctAudioEl.src = birdsData[theme][correct].audio;
+  console.log(birdsData[theme][correct].name);
+  return correctName;
+}
 
+function changeThemeBtnStyle(theme) {
+  themeBtns[theme].classList.remove("victorine-selector__theme--chosen");
+}
 
-loadVictorine()
+function updateAnswerInfo(variant) {
+  for (let i = 0; i < birdsData[theme].length; i++) {
+    if (birdsData[theme][i].name == variant) {
+      infoPic.src = birdsData[theme][i].image;
+      infoName.innerText = birdsData[theme][i].name;
+      infoSound.setAttribute("style", "visibility: visible");
+      infoSound.src = birdsData[theme][i].audio;
+    }
+  }
+}
+function infoUpdate(variant) {
+  updateAnswerInfo(variant);
+}
+
+function updateCorrectVictorine() {
+  for (let i = 0; i < birdsData[theme].length; i++) {
+    if (birdsData[theme][i].name == correctName) {
+      correctPicEl.src = birdsData[theme][i].image;
+      correctNameEl.innerText = birdsData[theme][i].name;
+    }
+  }
+}
+
+loadVictorine();
